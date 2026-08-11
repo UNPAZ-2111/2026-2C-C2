85e5d84d2d92c9f4d36eb13c3cadc0817df1b34f
Gus Del Dago <gdeldago@gmail.com>
2026-08-10 21:18:17 -0300

    Fin de la clase

---
+++ clase-1/Program.cs
@@ -0,0 +1,46 @@
+﻿// CONTROLADOR
+
+// Vamos a crear un objeto de clase "Jugador"
+// necesitamos una referencia para invocar al objeto
+
+Jugador player = new Jugador();
+
+// Ahora podemos invocar sus métodos(comportamientos)
+player.Mostrar();
+player.Atacar(10);
+player.Mostrar();
+player.RecibirAtaque(80);
+player.Mostrar();
+
+
+// MODELO
+
+// Declaramos y definimos la clase "Jugador"
+class Jugador 
+{
+    // Declaramos una "variable de clase" para cada atributo
+    // Si queremos podemos indicar su valor inicial (inicializamos)
+    private int vida = 100;
+    private int energia = 100;
+
+    // Declaramos y definimos una "función de clase" para cada
+    // Método (comportamiento) la semana próxima aclaramos
+
+    public void Atacar(int fuerza)
+    {
+        energia -= fuerza; // energia = energia - fuerza
+    }
+
+    public void RecibirAtaque(int fuerza)
+    {
+        vida -= fuerza; 
+    }
+
+
+    public void Mostrar()
+    {
+        Console.WriteLine($"Vida:{vida} Energia:{energia}");
+    }
+
+
+}
